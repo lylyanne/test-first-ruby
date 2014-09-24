9ea8b680d9f1b27f38ce55a6d9fabc52e4b26a48
Felicia Wang <felis_3007@yahoo.com>
2014-09-24 00:32:54 -0700

    Adding challenge 12

--- lib/12_rpn_calculator.rb
+++ lib/12_rpn_calculator.rb
@@ -1,3 +1,57 @@
 class RPNCalculator
-  # TODO: your code goes here!
+  def initialize
+  	@calc = []
+  end
+
+  def push(num)
+  	@calc << num.to_f
+  end
+
+  def plus
+  	operator(:+)
+  end
+
+  def minus
+  	operator(:-)
+  end
+
+  def divide
+  	operator(:/)
+  end
+
+  def times
+  	operator(:*)
+  end
+
+  def value
+  	@calc.last
+  end
+
+  def tokens(str)
+  	tokens_arr = []
+  	str.split(' ').each { |x| x.match(/\d/)!= nil ? tokens_arr << x.to_i : tokens_arr << x.to_sym }
+  	tokens_arr
+  end
+
+  def evaluate(str)
+  	str.split(" ").each { |x| 
+  		if x == "+" 
+  			plus
+  		elsif x == "*" 
+  		 	times
+  		elsif x == "-" 
+  		 	minus
+  		elsif x == "/" 
+  		 	divide
+  		else
+  			push(x)
+  		end
+  	}
+  	value
+  end
+
+  def operator(sym)
+  	raise ArgumentError.new('calculator is empty') if @calc.length < 2
+	@calc <<  @calc.pop(2).inject(&sym)
+  end
 end
